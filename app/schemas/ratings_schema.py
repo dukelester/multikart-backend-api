905b8f33eed8d1bd91cf4359baf6cb42ac719073
dukelester <dukelester4@gmail.com>
2022-09-16 00:10:59 +0300

    added the rating validations and tests

--- app/schemas/ratings_schema.py
+++ app/schemas/ratings_schema.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class RatingSchema(BaseModel):
     product_name: str
@@ -7,7 +7,24 @@ class RatingSchema(BaseModel):
     comment: str
     rated_at: datetime
     
+    @validator("product_name")
+    def validate_product_name(cls, product_name):
+        if type(product_name) != str or len(product_name) < 3 or len(product_name) > 100:
+            raise ValueError("Invalid Product name ")
+        return product_name
     
+    @validator('rating')
+    def validate_rating(cls,rating):
+        if type(rating) != int or rating < 0 or rating is None or rating > 10:
+            raise ValueError("This is an invalid rating")
+        return rating
+    
+    @validator("comment")
+    def validate_comment(cls, comment):
+        if type(comment) != str or len(comment) < 10 or " " not in comment or len(comment) > 1000:
+            raise ValueError("Invalid comment ..")
+        return comment
+
 class Rating(RatingSchema):
     id: int
     
--- app/tests/test_ratings.py
+++ app/tests/test_ratings.py
@@ -22,9 +22,30 @@ def test_creating_rating():
     
 def test_creating_rating_wrong_data():
     response = client.post("/ratings/",
-                           json={  
+                           json={ "product_name": " ",
                                 "rating": 4,
                                 "comment": "testing a rating",
                                 "rated_at": "2022-09-15T20:29:51.643Z",
                                 })
     assert response.status_code == 422
+    assert response.json() == {"detail": [ { "loc": ["body", "product_name" ], 
+                                            "msg": "Invalid Product name ", "type": "value_error" 
+                                            } ]
+                               }
+    
+def test_creating_rating_wrong_data_validations():
+    response = client.post("/ratings/",
+                           json={  
+                                "product_name": "hello there",
+                                "rating": 30,
+                                "comment": "product with a lot of nice features",
+                                "rated_at": "2022-09-15T20:29:36.908Z"
+                                })
+    assert response.status_code == 422
+    assert type(response.json()) == dict
+    assert response.json() == {"detail": [{ "loc": [ "body", "rating" ], 
+                                          "msg": "This is an invalid rating", "type": "value_error" 
+                                          }]
+                             }
+
+
